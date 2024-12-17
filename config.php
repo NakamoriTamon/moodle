05edbb0044738e29204c238ea3508da497b4fa32
t.nakamori <t.nakamori@trans-it.net>
2024-12-17 05:05:41 +0000

    2024/12/17 [change] 本番反映の為config.php修正

--- config.php
+++ config.php
@@ -4,23 +4,43 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'mysqli';
-$CFG->dblibrary = 'native';
-$CFG->dbhost    = 'db';
-$CFG->dbname    = 'moodle';
-$CFG->dbuser    = 'user';
-$CFG->dbpass    = 'password';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
-  'dbpersist' => 0,
-  'dbport' => 3306,
-  'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_0900_ai_ci',
-);
+if (in_array($_SERVER['HTTP_HOST'], ['localhost:8000', '127.0.0.1'])) {
+  $CFG->dbtype    = 'mysqli';
+  $CFG->dblibrary = 'native';
+  $CFG->dbhost    = 'db';
+  $CFG->dbname    = 'moodle';
+  $CFG->dbuser    = 'user';
+  $CFG->dbpass    = 'password';
+  $CFG->prefix    = 'mdl_';
+  $CFG->dboptions = array(
+    'dbpersist' => 0,
+    'dbport' => 3306,
+    'dbsocket' => '',
+    'dbcollation' => 'utf8mb4_0900_ai_ci',
+  );
 
-$CFG->wwwroot   = 'http://localhost:8000';
-$CFG->dataroot  = '/var/www/moodledata';
-$CFG->admin     = 'admin';
+  $CFG->wwwroot   = 'http://localhost:8000';
+  $CFG->dataroot  = '/var/www/moodledata';
+  $CFG->admin     = 'admin';
+} else {
+  $CFG->dbtype    = 'mysqli';
+  $CFG->dblibrary = 'native';
+  $CFG->dbhost    = 'osaka-univ-db.cisr8k3leqdh.ap-northeast-1.rds.amazonaws.com';
+  $CFG->dbname    = 'moodle';
+  $CFG->dbuser    = 'osakaunivdb';
+  $CFG->dbpass    = '5kAMlE2mVK3AeXOT3UTy';
+  $CFG->prefix    = 'mdl_';
+  $CFG->dboptions = array(
+    'dbpersist' => 0,
+    'dbport' => 3306,
+    'dbsocket' => '',
+    'dbcollation' => 'utf8mb4_unicode_ci',
+  );
+
+  $CFG->wwwroot   = 'https://open-univ.osaka-u.ac.jp';
+  $CFG->dataroot  = '/var/www/moodledata';
+  $CFG->admin     = 'admin';
+}
 
 $CFG->slasharguments = 0;
 $CFG->directorypermissions = 0777;
