beb4bf7ec09f6db423421653f425dee961a8e186
t.nakamori <t.nakamori@trans-it.net>
2025-03-11 11:28:31 +0000

    [add] 2025/03/11 中森 既存適塾記念会会員リスト出力機能作成

--- custom/admin/app/Controllers/management/tekijuku_upsert_controller.php
+++ custom/admin/app/Controllers/management/tekijuku_upsert_controller.php
@@ -18,6 +18,31 @@ try {
     ");
     $max_number = $max_number->number + 1;
 
+    // CSVヘッダー
+    $csv_list[0] = [
+        '会員番号',
+        '会員種別',
+        '氏名',
+        'カナ',
+        '部局名',
+        '学科・専攻名',
+        '職名',
+        '郵便番号',
+        '住所',
+        '電話番号',
+        '『適塾』氏名掲載不可',
+        '備考',
+        '2024（R6)',
+        '2025（R7)',
+        '2026（R8)',
+        '2027（R9)',
+        '2028（R10)',
+        '2029（R11)',
+        '2030（R12)',
+        'メールアドレス(ログイン用)',
+        'パスワード(ログイン用)',
+    ];
+
     // CSVファイルに変換した会員情報を取得する
     if ($_FILES['csv_file']['error'] === UPLOAD_ERR_OK) {
         $file = $_FILES['csv_file']['tmp_name'];
@@ -27,6 +52,7 @@ try {
             $columns = array_map(fn($val) => mb_convert_encoding($val, 'UTF-8', 'SJIS-win'), $columns);
             while (($data = fgetcsv($handle)) !== false) {
                 $count = $count + 1;
+                $csv_array = [];
                 $data = array_map(fn($val) => mb_convert_encoding($val, 'UTF-8', 'SJIS-win'), $data);
                 $params = array_combine($columns, $data);
                 if ($params['会員番号'] == '') {
@@ -94,7 +120,7 @@ try {
                 }
 
                 // 枚数(仮)
-                $unit = 1;
+                $unit = $params['2024（R6)'];
                 $post_code = !empty($params['郵便番号']) ?  str_replace('-', '', $params['郵便番号']) : '';
                 $is_published = !empty($params['『適塾』氏名掲載不可 ']) ? true : false;
                 $note = $params['備考'];
@@ -109,7 +135,39 @@ try {
                 $is_deposit_2028 = !empty($params['2028（R10)']) ? true : false;
                 $is_deposit_2029 = !empty($params['2029（R11)']) ? true : false;
                 $is_deposit_2030 = !empty($params['2030（R12)']) ? true : false;
+                $is_university_member = false;
+                if (!empty($department) || !empty($major) || !empty($official)) {
+                    $is_university_member = true;
+                }
                 $max_number = $max_number + 1;
+
+                // CSVデータ取得
+                $csv_array = [
+                    $old_number,
+                    $params['会員種別'],
+                    $name,
+                    $kana,
+                    $department,
+                    $major,
+                    $official,
+                    $post_code,
+                    $address,
+                    $params['電話番号'],
+                    $params['『適塾』氏名掲載不可 '],
+                    $note,
+                    $params['2024（R6)'],
+                    $params['2025（R7)'],
+                    $params['2026（R8)'],
+                    $params['2027（R9)'],
+                    $params['2028（R10)'],
+                    $params['2029（R11)'],
+                    $params['2030（R12)'],
+                    $email,
+                    $password,
+                ];
+
+
+                $csv_list[$count - 1] = $csv_array;
 
                 // 適塾記念会会員情報登録
                 $tekijuku_commemoration = new stdClass();
@@ -120,7 +178,7 @@ try {
                 $tekijuku_commemoration->name = $name;
                 $tekijuku_commemoration->kana = $kana;
                 $tekijuku_commemoration->post_code = rtrim($post_code, " 　");
-                $tekijuku_commemoration->address = $adress;
+                $tekijuku_commemoration->address = $address;
                 $tekijuku_commemoration->tell_number = $phone;
                 $tekijuku_commemoration->email = $email;
                 $tekijuku_commemoration->is_published = $is_published;
@@ -140,7 +198,7 @@ try {
                 $tekijuku_commemoration->is_temporary = 1;
                 $tekijuku_commemoration->price = $price;
                 $tekijuku_commemoration->unit = $unit;
-                $tekijuku_commemoration->sex = 0; // 消すそのうち
+                $tekijuku_commemoration->is_university_member = $is_university_member;
 
                 $DB->insert_record_raw('tekijuku_commemoration', $tekijuku_commemoration);
             }
@@ -151,6 +209,45 @@ try {
     } else {
         echo "ファイルのアップロードに失敗しました。";
     }
+
+    // 保存先のファイルパス
+    $save_path = "/var/www/html/moodle/uploads/output.csv";
+
+    if (!is_writable(dirname($save_path))) {
+        die("ディレクトリに書き込み権限がありません: " . dirname($save_path));
+    }
+
+    // ディレクトリがない場合は作成
+    if (!is_dir(dirname($save_path))) {
+        mkdir(dirname($save_path), 0777, true);
+    }
+
+    // ファイルを開く
+    $fp = fopen($save_path, "w");
+    if ($fp === false) {
+        die("ファイルを開けませんでした");
+    }
+
+    // データをCSVとして書き込み（カンマ区切り）
+    foreach ($csv_list as $row) {
+        $row = array_map(function ($val) {
+            // 文字列がUTF-8でない場合にUTF-8に変換
+            if (!mb_detect_encoding($val, "UTF-8", true)) {
+                $val = mb_convert_encoding($val, "UTF-8");
+            }
+
+            // Shift-JISに変換
+            return mb_convert_encoding($val, "SJIS-win", "UTF-8");
+        }, $row);
+
+        fputcsv($fp, $row);
+    }
+
+    fclose($fp);
+
+    echo "CSVファイルを作成しました: <a href=/uploads/output.csv>ダウンロード</a>";
+
+
     $transaction->allow_commit();
     $_SESSION['message_success'] = '登録が完了しました';
     header('Location: /custom/admin/app/Views/management/tekijuku_registration.php');
