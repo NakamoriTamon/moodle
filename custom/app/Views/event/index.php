4add411062dc2e7aece514f56814a4e487ed57be
shibuya <shibuya@trans-it.net>
2025-03-13 10:24:47 +0000

    イベント一覧is_topが1のデータのみバッジをつけるよう変更

--- custom/app/Views/event/index.php
+++ custom/app/Views/event/index.php
@@ -118,7 +118,7 @@ unset($SESSION->formdata);
             <ul class="result_list" id="event">
                 <?php if (isset($events) && !empty($events)): ?>
                     <?php foreach ($events as $row): ?>
-                        <li class="event_item rec">
+                        <li class="<?php echo ($row['is_top'] === 1) ? 'rec ' : ''; ?>event_item">
                             <a href="/custom/app/Views/event/detail.php?id=<?= $row['id'] ?>">
                                 <figure class="img"><img src="<?= htmlspecialchars(empty($row['thumbnail_img']) ? DEFAULT_THUMBNAIL : $row['thumbnail_img']); ?>" alt="" /></figure>
                                 <div class="event_info">
