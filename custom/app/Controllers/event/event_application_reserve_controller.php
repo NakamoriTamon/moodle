51f6e0b0058b7dd5c63af6b85176480714f68650
t.nakamori <t.nakamori@trans-it.net>
2025-03-14 15:35:53 +0000

    [fix] 2025/03/15 中森 イベント予約お連れ様のメールアドレス修正

--- custom/app/Controllers/event/event_application_reserve_controller.php
+++ custom/app/Controllers/event/event_application_reserve_controller.php
@@ -16,7 +16,7 @@ class EventReserveController
 
     public function index($course_id)
     {
-
+        global $USER;
         $histry_list = $this->eventApplicationCourseInfoModel->getByCourseInfoId($course_id, null);
 
         // 各種情報を切り分ける
@@ -29,20 +29,23 @@ class EventReserveController
         $is_payment = empty($common_applicationt['payment_date']) ? '未決済' : '決済済';
         $child_name = $common_application['companion_name'] ?? $common_application['user']['child_name'];
 
-
         $companion_array = [];
         foreach ($histry_list as $histry) {
-            $companion_array[] = $histry['participant_mail'];
+            foreach ($histry['application'] as $application) {
+                if ($USER->id == $application['user_id']) {
+                    $target_id = $application['id'];
+                    break;
+                }
+            }
         }
+
+        $companion_email_array = $this->eventApplicationCourseInfoModel->getByApplicationId($target_id, $course_id);
 
         // 本人のメールアドレスは排除
         $email = $common_application['email'];
-        $companion_array = array_filter($companion_array, function ($item) use ($email) {
-            return $item !== $email;
+        $companion_array = array_filter($companion_email_array, function ($item) use ($email) {
+            return $item['participant_mail'] !== $email;
         });
-
-        // インデックスが再割り当てされているので、必要なら再インデックス化する
-        $companion_array = array_values($companion_array);
 
         $data = [
             'common_array' => $common_array,
