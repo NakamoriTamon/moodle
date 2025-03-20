c4e6df7291eee8af354ec4f803aea3d1f4820d8f
t.nakamori <t.nakamori@trans-it.net>
2025-03-20 09:22:21 +0000

    [change] 2025/03/20 中森 新規登録処理調整

--- custom/admin/app/Views/signup/signup_confirm.php
+++ custom/admin/app/Views/signup/signup_confirm.php
@@ -3,47 +3,33 @@ require_once('/var/www/html/moodle/config.php');
 require_once($CFG->dirroot . '/custom/admin/app/Controllers/login/admin_registration_controller.php');
 $admin_registration_controller = new adminRegistrationController();
 $result = $admin_registration_controller->index($_GET['id'], $_GET['expiration_time']);
+include('/var/www/html/moodle/custom/admin/app/Views/common/logon_header.php');
 ?>
 
-<?php include('/var/www/html/moodle/custom/app/Views/common/header.php'); ?>
-<link rel="stylesheet" type="text/css" href="/custom/public/assets/css/form.css" />
-
-<main id="subpage">
-    <section id="heading" class="inner_l">
-        <?php if ($result) { ?>
-            <h2 class="head_ttl" data-en="REGISTRATION SUCCESSFUL">本登録完了</h2>
-        <?php } else { ?>
-            <h2 class="head_ttl" data-en="REGISTRATION FATAL">本登録失敗</h2>
-        <?php } ?>
-    </section>
-    <!-- heading -->
-
-    <div class="inner_l">
-        <section id="form" class="complete">
-            <ul id="flow">
-                <li>入力</li>
-                <li class="active">完了</li>
-            </ul>
-            <div class="whitebox form_cont">
-                <p class="cpt_txt"><?= $result ? "本登録が完了いたしました。" : "本登録に失敗しました。" ?></p>
-                <p class="sent">
-                    <?= $result ? "本登録が完了いたしました。" : "本登録に失敗しました。" ?><br />
-                    <?php if ($result) { ?>
-                        ログイン画面からログインして<br class="pc" />システムをご利用ください
-                    <?php } else { ?>
-                        再度手続きしてください。<br class="pc" />
-                    <?php } ?>
-                </p>
+<body id="login" data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
+    <main class="d-flex w-100 h-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
+                        <div class="text-center mt-4 mb-3">
+                            <h1 class="h2"><?= $result ? "本登録が完了いたしました。" : "本登録に失敗しました。" ?></h1>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-3">
+                                    <div class="d-grid gap-2 mt-3">
+                                        <a href="/custom/admin/app/Views/login/login.php" class="btn btn-lg btn-outline-dark">ログイン画面へ</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <a href="../index.php" class="btn btn_blue arrow box_bottom_btn">TOPへ戻る</a>
-        </section>
-        <!-- contact -->
-    </div>
-</main>
+        </div>
+        </div>
+    </main>
+</body>
 
-<ul id="pankuzu" class="inner_l">
-    <li><a href="../index.php">トップページ</a></li>
-    <li>お問い合わせ</li>
-</ul>
-
-<?php include('/var/www/html/moodle/custom/app/Views/common/footer.php'); ?>
+</html>
