0e7571ab7eafd682434dbabd3c01fc1e7b06de47
yasudashigeki <yasuda@trans-it.net>
2025-03-25 09:05:18 +0000

    2025/3/25　保田　申込画面でチケット申込：0の場合にエラーとなるのを修正

--- custom/app/Controllers/event/event_application_controller.php
+++ custom/app/Controllers/event/event_application_controller.php
@@ -99,13 +99,20 @@ class EventApplicationController
 
         $fieldList = $this->eventCustomFieldModel->getCustomFieldById($event['event_customfield_category_id']);
         // 空数確認
-        $ticket_data = $this->eventApplicationModel->getSumTicketCountByEventId($eventId, empty($courseInfoId) ? null : $courseInfoId, true)[0];
-
-        if($event['capacity'] == 0) {
-            $aki_ticket = 0;
+        // $courseInfoIdが無い場合、空数が最小のレコードを取得
+        // $courseInfoIdが有る場合、指定した開催日のレコードを取得
+        $result = $this->eventApplicationModel->getSumTicketCountByEventId($eventId, empty($courseInfoId) ? null : $courseInfoId, true);
+        if(!empty($result)) {
+            $ticket_data = $result[0];
+            if($event['capacity'] == 0) {
+                $aki_ticket = 0;
+            } else {
+                $aki_ticket = $ticket_data['available_tickets'];
+            }
         } else {
-            $aki_ticket = $ticket_data['available_tickets'];
+            $aki_ticket = 0;
         }
+
 
         $cognitions = $this->cognitionModel->getCognition();
         $paymentTypes = $this->paymentTypeModel->getPaymentTypes();
