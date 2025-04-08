5ef80a97d0df2bb88ddd53fd244ecc830f8bd1fa
yasudashigeki <yasuda@trans-it.net>
2025-04-08 08:06:57 +0000

    2025/4/8 保田　マイページの予約一覧で毎日開催の表記を変更

--- custom/app/Controllers/event/event_application_reserve_controller.php
+++ custom/app/Controllers/event/event_application_reserve_controller.php
@@ -42,8 +42,15 @@ class EventReserveController
         $realtime_path = $common_application['event']['real_time_distribution_url'];
         $event_name =  $no . $common_application['event']['name'];
         $price = $common_application['price'] != 0 ? number_format($common_application['price']) . '円' : '無料';
-        $pay_method = PAYMENT_SELECT_LIST[$common_application['pay_method']];
-        $is_payment = empty($common_application['payment_date']) ? '未決済' : '決済済';
+
+        if($common_application['price'] != 0) {
+            $pay_method = PAYMENT_SELECT_LIST[$common_application['pay_method']];
+            $is_payment = empty($common_application['payment_date']) ? '未決済' : '決済済';
+        } else {
+            $pay_method = "";
+            $is_payment = "";
+        }
+        
         $child_name = $common_application['companion_name'] ?? $common_application['user']['child_name'];
 
         $companion_array = [];
