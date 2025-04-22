71234db5c2d5bee8b6c499943f4382cbacf3e346
yasudashigeki <yasuda@trans-it.net>
2025-04-22 07:46:26 +0000

    2025/4/22 保田　アンケートが１つでも回答されていたら追加、削除できないように変更

--- custom/admin/app/Controllers/survey/survey_custom_controller.php
+++ custom/admin/app/Controllers/survey/survey_custom_controller.php
@@ -2,15 +2,21 @@
 require_once('/var/www/html/moodle/config.php');
 require_once($CFG->dirroot . '/custom/app/Models/BaseModel.php');
 require_once($CFG->dirroot . '/custom/app/Models/EventSurveyCustomFieldCategoryModel.php');
+require_once($CFG->dirroot . '/custom/app/Models/SurveyApplicationModel.php');
+require_once($CFG->dirroot . '/custom/app/Models/SurveyApplicationCustomfieldModel.php');
 
 class SurveyCustomController
 {
 
     private $surveyCustomFieldCategoryModel;
+    private $surveyApplicationModel;
+    private $surveyApplicationCustomfieldModel;
 
     public function __construct()
     {
         $this->surveyCustomFieldCategoryModel = new EventSurveyCustomFieldCategoryModel();
+        $this->surveyApplicationModel = new SurveyApplicationModel();
+        $this->surveyApplicationCustomfieldModel = new SurveyApplicationCustomfieldModel();
     }
 
     public function index()
@@ -32,6 +38,22 @@ class SurveyCustomController
             $_SESSION['message_error'] = '選択したカスタムフィールドは存在しません ';
             redirect(new moodle_url('/custom/admin/app/Views/survey/custom_index.php'));
             exit;
+        }
+        $events = $surveyCustomFields['event'];
+        
+        // アンケートがあるか確認
+        $surveyCustomFields['answer'] = false;
+        foreach ($events as $event) {
+            $event_id = $event['id'];
+            $surveyApplication = $this->surveyApplicationModel->getSurveyApplications(null, $event_id, 1 ,1);
+
+            if(!empty($surveyApplication)) {
+                $answer = $this->surveyApplicationCustomfieldModel->getESurveyApplicationCustomfieldBySurveyApplicationId($surveyApplication[0]['id']);
+                if(!empty($answer)) {
+                    $surveyCustomFields['answer'] = true;
+                    break;
+                }
+            }
         }
         return $surveyCustomFields;
     }
