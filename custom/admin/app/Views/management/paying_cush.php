02746a0fb81bef665a2ca93d334d257ff21d7c42
t.nakamori <t.nakamori@trans-it.net>
2025-04-25 22:33:23 +0900

    [fix] 2025/04/25 中森 文言修正

--- custom/admin/app/Views/management/paying_cush.php
+++ custom/admin/app/Views/management/paying_cush.php
@@ -377,7 +377,7 @@ function determinePaymentStatus($tekijuku_commemoration, $current_fiscal_year)
                                     </div>
                                     <div class="mb-3">
                                         <input class="checkbox_input" type="checkbox" name="is_university_member" id="is_university_member" value="1" <?php echo ($old_input['is_university_member'] ?? $tekijuku_commemoration['is_university_member']) == '1' ? 'checked' : ''; ?>>
-                                        <label class="checkbox_label" id="is_university_member_label" for="is_university_member">大阪大学教職員・学生の方はこちらにチェックしてください。</label>
+                                        <label class="checkbox_label" id="is_university_member_label" for="is_university_member">大阪大学教職員の方はこちらにチェックしてください。</label>
                                     </div>
                                     <div id="university_member_fields">
                                         <div class="mb-3">
