86b253d9c01a0d3997733efbd40bbf7c661dd42e
t.nakamori <t.nakamori@trans-it.net>
2025-05-15 15:55:49 +0900

    [add] 2025/05/15 中森 適塾記念会文言追加

--- custom/app/Views/tekijuku/index.php
+++ custom/app/Views/tekijuku/index.php
@@ -84,7 +84,8 @@ $isTekijukuCommemorationMember = $tekijuku_index_controller->isTekijukuCommemora
                 <h2 class="block_ttl">会員証</h2>
                 <p class="sent">
                     入会いただいた方には、デジタル会員証を発行いたします（紙での発行はございません）。<br />
-                    入会された当該年度（３月末まで）有効で、適塾の受付でご提示いただくと、適塾（「適塾特別展示」を含む）を何度でもご参観いただけます。
+                    入会された当該年度（３月末まで）有効で、適塾の受付でご提示いただくと、適塾（「適塾特別展示」を含む）を何度でもご参観いただけます。<br />
+                    なお、デジタル会員証は、紙に印刷したものやスクリーンショットでもご参観いただけます。
                 </p>
             </div>
             <div class="juku_block membership">
