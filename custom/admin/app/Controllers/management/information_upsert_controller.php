81f1431670c3d6f7129d619f00be9620662cd58d
transonic\TransonicSoftware <t.ishikawa@trans-it.net>
2025-06-12 09:43:19 +0000

    [fix] 2025/06/12 石川 修正6

--- custom/admin/app/Controllers/management/information_upsert_controller.php
+++ custom/admin/app/Controllers/management/information_upsert_controller.php
@@ -15,22 +15,15 @@ $publish_end_at  = $_POST['publish_end_at'] ?? null;
 try {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (empty($_POST['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token']) || empty($USER->id)) {
-            var_dump("テストcsrf");
             $_SESSION['message_error'] = '登録に失敗しました';
             if ($id) {
-                 var_dump("テストid");
-                 exit;
                 header('Location: /custom/admin/app/Views/management/information_upsert.php?id=' . $id);
             } else {
-                   var_dump("テスト新規");
-                 exit;
                 header('Location: /custom/admin/app/Views/management/information_upsert.php');
             }
             exit;
         }
     }
-    var_dump("テストfirst");
-    exit;
     $hasError = false;
     if (empty($title)) {
         $_SESSION['errors']['title'] = '件名は必須です';
@@ -134,7 +127,8 @@ try {
         $information->publish_end_at = $publish_end_at ? date('Y-m-d H:i:s', strtotime($publish_end_at)) : null;
         $DB->update_record('information', $information);
     }
-
+    var_dump("テスト3");
+    exit;
     $transaction->allow_commit();
     $_SESSION['message_success'] = '登録が完了しました';
     header('Location: /custom/admin/app/Views/management/information.php');
